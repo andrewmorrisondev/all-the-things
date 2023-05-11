600218837a50db2722aefcb4a8f9dcbe5c2b88e6
nonchalamment <nonchalamment@gmail.com>
2023-05-11 16:46:23 -0400

    change banana to not-orange

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Landing from './pages/Landing/Landing'
 const App = () => {
   const bensThings = [
     {
-      name: 'banana',
+      name: 'not-orange',
       image:
         'https://images.unsplash.com/photo-1587132137056-bfbf0166836e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       attributes: [
