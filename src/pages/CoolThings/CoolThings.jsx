097e99cc04db47ccd950036faab20e0953996226
trentonwahr <trentwahr@gmail.com>
2023-05-11 16:15:20 -0400

    add more cool things

--- src/pages/CoolThings/CoolThings.jsx
+++ src/pages/CoolThings/CoolThings.jsx
@@ -5,6 +5,7 @@ const CoolThings = (props) => {
   return (
     <>
       <h1>Trenton's Things</h1>
+      <h2>Testing stuff</h2>
       <Link to="/">Home</Link>
 
       {props.things.map((thing, idx) => 
